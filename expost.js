807d742d6ac3f84b9171012158568e694ff4c36d
Bethany Soule <bsoule@gmail.com>
2011-02-11 13:31:47 -0600

    refactoring, and expost defaults to post preview now, if available. (instead of 'source', i.e. the etherpad)

--- expost.js
+++ expost.js
@@ -1,52 +1,57 @@
 jQuery(document).ready(function($) {
-
   $('#titlediv').after($('#expost_div').detach());
 
   $('#ep_preview').click( function() {
-    $('#ep_preview').addClass('orange').removeClass('white');
-    $('#ep_source').removeClass('orange').addClass('white');
-    $('#expost-content-frame').remove();
-    // $('#expost_content')
-    //   .append( '<div id="expost-content-preview"> the content </div>' );
-    $.post( ajaxurl, 
+
+      $.post( ajaxurl, 
           { action: "expost_fetch_post",
             source: $('#expost_source').val() },
             function(response) {
-              if (response.post_preview) {
+              if ($.trim(response.post_preview)) {
+
+                $('#ep_preview').addClass('orange').removeClass('white');
+                $('#ep_source').removeClass('orange').addClass('white');
+                $('#expost-content-frame').remove();
+ 
                 if ( !$('#expost-content-preview').length ) 
                   $('#expost_content')
                     .append('<div id="expost-content-preview"></div>');
                 $('#expost-content-preview').html(response.post_preview); 
+
+              } else {
+                $('#ep_source').click();
               } 
             }, 
           "json" );
   } );
 
-  $('#ep_source').parent().click( function() {
-    $('#expost_source').change();
-  } );
+  $('#ep_source').click( function() {
 
-  $('#expost_source').bind( 'change keypress', function(e) {
-    // key codes: tab=9, enter=13, undefined=>'change'
-    var key = (e.keyCode || 0), s = $('#expost_source');
+    var s = $('#expost_source');
     $('#ep_source').addClass('orange').removeClass('white');
     $('#ep_preview').removeClass('orange').addClass('white');
 
+    if ( $('#expost-content-frame').length ) {
+      $('#expost-content-frame')
+        .attr('src', s.val());   
+    } else {
+      $('#expost-content-preview').remove();
+      $('#expost_content')
+        .append( "<iframe id='expost-content-frame' src='"+
+                s.val()+"' /></div>"); 
+    } 
+  });
+
+  $('#expost_source').bind( 'change keypress', function(e) {
+    // key codes: tab=9, enter=13, undefined=>'change'
+    var key = (e.keyCode || 0), s = $('#expost_source'); 
+
     if ( 13 == key || 9 == key || 'change' == e.type ) { // 'save' ==> 
-      if (s.val()) {
+      if (s.val()) { // TODO: s.val() ?== TRUE if s.val() = '' 
         $('#postdivrich').hide();
         $('#expost_buttons').show();
-
-        if ( $('#expost-content-frame').length ) {
-          $('#expost-content-frame')
-            .attr('src', s.val());   
-        } else {
-          $('#expost-content-preview').remove();
-          $('#expost_content')
-            .append( "<iframe id='expost-content-frame' src='"+
-                    s.val()+"' /></div>"); 
-        } 
-        return false;
+        //TODO: update whichever is active
+        $('#ep_preview').click();
       } else {
         $('#expost-content-frame').remove();
         $('#expost-content-preview').remove();
@@ -54,8 +59,7 @@ jQuery(document).ready(function($) {
         $('#expost_buttons').hide();
         return false;
       }
-    } // if ( 27 == key ) // 'escape': don't do anything special
-    
+    } 
   });
 
   if ($('#expost_source').val()) {
