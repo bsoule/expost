557fad1596f7ad1af13005bf3565d4e49677d670
Nathan Arthur <nathan@nathanarthur.com>
2024-05-07 10:53:39 -0400

    delete commented code

--- eslint.config.js
+++ eslint.config.js
@@ -20,21 +20,3 @@ export default tseslint.config(
     },
   }
 );
-
-// export default [
-//   js.configs.recommended,
-//   {
-//     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-//     parser: "@typescript-eslint/parser",
-//     plugins: ["@typescript-eslint"],
-//     root: true,
-//     overrides: [
-//       {
-//         files: ["vitest.setup.ts", "*.spec.ts", "src/lib/test/**/*.ts"],
-//         rules: {
-//           "@typescript-eslint/no-explicit-any": "off",
-//         },
-//       },
-//     ],
-//   },
-// ];
