139ef9eff08339b77b868a969dbead606923e971
Jake Coble <j@kecoble.com>
2024-05-28 17:46:28 -0400

    Add title parsing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { body } from "./schemas/body.js";
 
 export function parseMarkdown(markdown: string): string {
-  return body.parse(markdown);
+    return body.parse(markdown);
+}
+
+export function parseTitle(content: string): string | undefined {
+    return content.match(/BEGIN_MAGIC\[(.*?)\]/)?.[1];
 }
